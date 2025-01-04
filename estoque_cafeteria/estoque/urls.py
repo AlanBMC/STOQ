fe6bd774f7cf4cf3ff0e2e68ccb59ed84b9990d0
Alan Morais <alanbmc00@gmail.com>
2025-01-04 16:29:40 -0400

    feat: template confi e alerta global

--- estoque_cafeteria/estoque/urls.py
+++ estoque_cafeteria/estoque/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('produtoview/', views.produtoview, name='produtoview'),
     path('estoqueview/', views.estoqueview, name='estoqueview'),
-    path('offline/', views.offline, name='offline')  # Nova rota offline
+    path('configuracaoview/', views.configuracaoview, name='configuracaoview'),
+    path('offline/', views.offline, name='offline')  
 
 ]
  
--- estoque_cafeteria/estoque/views.py
+++ estoque_cafeteria/estoque/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+import json
+
 
 # Create your views here.
 def login(request):
@@ -12,3 +18,10 @@ def estoqueview(request):
 
 def offline(request):
     return render(request, 'offline.html')
+
+def configuracaoview(request):
+    return render(request, 'configuracao.html')
+
+def cria_usuario(request):
+    
+    pass
