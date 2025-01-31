b5f95d3927d30103005387c584d15b94830633b4
Alan Morais <alanbmc00@gmail.com>
2025-01-31 17:36:28 -0400

    feat: update logo handling, modify manifest details, and enhance CSS for responsive design

--- estoque_cafeteria/estoque/static/sw.js
+++ estoque_cafeteria/estoque/static/sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'estoque-cache-v4';
 const urlsToCache = [
     '/',
     '/static/css/global.css',
-    '/static/images/logo192.png',
+    '/static/images/maskable_icon_x128 (2).png',
     '/templates/offline.html'
 ];
 
